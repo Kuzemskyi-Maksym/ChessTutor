8cae2a5feb94a24cdaac7a28da4502cef69c81cd
Kuzemskyi-Maksym <maksymkuzemskyi@gmail.com>
2026-04-30 21:30:47 +0300

    Add project files.

---
+++ Logic/MoveValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessTutor.Logic
+{
+    internal class MoveValidator
+    {
+    }
+}
